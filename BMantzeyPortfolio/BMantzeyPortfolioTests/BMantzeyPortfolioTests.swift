3610a8e3ed5a7452a8dec788b712260a9ca22414
Brandon Mantzey <bmantzey@BrandonacStudio.lan>
2024-10-11 15:33:23 -0500

    New Project Creation

---
+++ BMantzeyPortfolio/BMantzeyPortfolioTests/BMantzeyPortfolioTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMantzeyPortfolioTests.swift
+//  BMantzeyPortfolioTests
+//
+//  Created by Brandon Mantzey on 10/11/24.
+//
+
+import Testing
+@testable import BMantzeyPortfolio
+
+struct BMantzeyPortfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
